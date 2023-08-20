b43588d7d49f2468f3232c0b1bc8f4d5006774a5
elahmedi <elahmedi89@gmail.com>
2023-08-20 13:24:02 -0400

    added the rest of the full texts

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ped-surg-ai
-QA model built on Langchain and GPT3.5
+This is a Question-Answer model built on Langchain and GPT3.5. It answers questions based on the 112 studies that were included in Elahmedi et al. systematic review on AI in pediatric surgery
+
+1. Install the required dependencies
+2. Export your OpenAI API key
+3. Run the model
---
+++ qa.py
@@ -0,0 +1,40 @@
+# Step 1 Load
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+loader = PyPDFDirectoryLoader("pdf")
+docs = loader.load()
+
+# Step 2 Split
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000, chunk_overlap = 200)
+all_splits = text_splitter.split_documents(docs)
+
+# Step 3 Store
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+
+# Step 4 Retrieve
+question = input("enter your question please: ")
+docs = vectorstore.similarity_search(question)
+#len(docs)
+
+# Step 5 Generate
+# from langchain.chains import RetrievalQAWithSourcesChain
+#from langchain.chat_models import ChatOpenAI
+#
+#llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+#qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm,retriever=vectorstore.as_retriever())
+#result = qa_chain({"query": question})
+#print(result)
+
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
+qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectorstore.as_retriever())
+print(qa_chain({"query": question},return_only_outputs=True))
+
+# Step 6 Memory
+# Will be added in the future
